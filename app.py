722b1b2aafd33635d93aae59e9d165469b53e113
KUNU1PRADEEP <114349142+KUNU1PRADEEP@users.noreply.github.com>
2023-11-11 19:58:49 +0000

    finally, nearer to deployment

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template,jsonify
 
 app=Flask(__name__)
 
+JOBS=[
+
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Bengaluru',
+    'salary':'$100,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'delhi,India ',
+    'salary':'$100,000'
+  },
+  {
+    'id':3,
+    'title':'Frontend Engineer',
+    'location':'Remote',
+    
+  },
+  {
+    'id':4,
+    'title':'Backend Engineer',
+    'location':'San Francisco, USA',
+    'salary':'$120,000'
+  }
+]
+
 @app.route("/")
-def hello_world():
-  return "Hello, world"
+# def hello_world():
+#   return "Hello, world"
+
+def hello_kunal():
+  return render_template('home.html',jobs=JOBS,company_name='Kunal')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__=="__main__":
   app.run(host='0.0.0.0' ,debug=True)
